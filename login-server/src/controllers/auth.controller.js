1f0181646df7f988547ad6fc1487b999b1ee4ccb
Serhii Gramma <grammas56@gmail.com>
2024-03-21 13:45:37 +0200

    server user registration validation

--- login-server/src/controllers/auth.controller.js
+++ login-server/src/controllers/auth.controller.js
@@ -1,10 +1,43 @@
 import { User } from "../models/user.js";
 import { userService } from '../services/user.service.js';
 import { jwtService } from '../services/jwt.service.js';
+import { ApiError } from "../exeptions/api.error.js";
+
+function validateEmail(value) {
+  if (!value) {
+    return 'Email is required';
+  }
+
+  const emailPattern = /^[\w.+-]+@([\w-]+\.){1,3}[\w-]{2,}$/;
+
+  if (!emailPattern.test(value)) {
+    return 'Email is not valid';
+  }
+}
+
+function validatePassword(value) {
+  if (!value) {
+    return 'Password is required';
+  }
+    
+  if (value.length < 6) {
+    return 'At least 6 characters';
+  }
+};
+
 
 const register = async (req, res, next) => {
   const { email, password } = req.body;
   
+  const errors = {
+    email: validateEmail(email),
+    password: validatePassword(password)
+  }
+
+  if (errors.email || errors.password) {
+    throw ApiError.badRequest('Bad request', errors)
+  }
+
   await userService.register(email, password)
   res.send({message: 'OK'});
 }
